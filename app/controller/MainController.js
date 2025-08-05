fd5e22a40de53930b1436ed8bdfef4ade77d5d71
Gia Phong <ngphong.7800@gmail.com>
2025-08-05 22:10:39 +0700

    update

--- app/controller/MainController.js
+++ app/controller/MainController.js
@@ -8,22 +8,11 @@ Ext.define("MyApp.controller.MainController", {
       "ThemTapPopup_View #openPopupButton": {
         click: this.onOpenPopup,
       },
-      Popup_View: {
-        save: this.onDataSaved,
-      },
     });
   },
 
   onOpenPopup: function () {
-    const popup = Ext.create("MyApp.view.HoSo.Popup.Popup_View");
-    popup.on("save", this.onDataSaved, this);
+    var popup = Ext.create("MyApp.view.HoSo.Popup.Popup_View");
     popup.show();
-  },
-
-  onDataSaved: function (values) {
-    const grid = Ext.ComponentQuery.query("ThemTapPopup_View")[0];
-    if (grid && grid.addDataToTable) {
-      grid.addDataToTable(values);
-    }
   },
 });
